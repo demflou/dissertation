a8cbb6422dc52e48ffe512cf15d606ae4631fe98
demflou <demflou@gmail.com>
2020-05-03 02:40:41 +0300

    Classification

--- README.md
+++ README.md
@@ -16,4 +16,5 @@ Distributed Task Management at the Edge of the Network
 
 # Feature Learning
 I classify the given data from [d0, dcolumns-1]. The last columns I implement it as a label with values 0 or 1.
+Use Chi-square test to check how much important is each feature to y and return the k-most important features.
 Then i use SelectKbest function to predict the most important dimensions of the row so i will check them later for import a new row into a node.
--- Simulation.py
+++ Simulation.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
-row_per_node = 100
-important_rows = 2
+rows_per_node = 100
+important_cols = 2
 
 class dNode:
     def __init__(self, n, df, noc, nor):
@@ -13,6 +13,7 @@ class dNode:
         self.data = df.copy()
         self.cntr = nor
         self.avg_ = [-1] * noc
+        self.dScore = [-1] * important_cols
         calc_avg(self) #get the avg of imported data
         #self.similarity = [-1] * non
 
@@ -23,6 +24,7 @@ def printNode (dn):
     #print ('-'*100)
     #print (dn.data)
     print ('AVG: ', dn.avg_)
+    print ('Score: ', dn.dScore)
     #print ('SIMILARITY: ', dn.similarity)
 
 def calc_avg(dn):
@@ -31,6 +33,24 @@ def calc_avg(dn):
     for i in range (c):
         dn.avg_[i] = res[i]
     return dn
+
+def classify_df(dn, noc, nor):
+    # DATA CONVERTING
+
+    # Convert the data to be able to classify
+    X = dn.data.iloc[:, 0:noc-2]
+    y = dn.data.iloc[:, noc-1]
+
+    # Extract the 2 most important columns
+    topD = SelectKBest(score_func=chi2, k=important_cols)
+    fit = topD.fit(X, y)
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(X.columns)
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Dimensions', 'Score']  # naming the dataframe columns
+    temp = featureScores.nlargest(important_cols, 'Score')  # print n best features
+    for i in range(important_cols):
+        dn.dScore[i] = temp['Dimensions'].index[i]
 
 def main(argv):
     columns = -1
@@ -69,34 +89,14 @@ def main(argv):
 
     # Split 100 rows at each node
     for n in range(num_of_nodes):
-        t_df = pd.DataFrame(dfData.iloc[(n*row_per_node)+1:((n+1)*row_per_node)+1,:])
-        ListNodes_.append(dNode(n, t_df, columns, row_per_node))
+        t_df = pd.DataFrame(dfData.iloc[(n*rows_per_node)+1:((n+1)*rows_per_node)+1,:])
+        ListNodes_.append(dNode(n, t_df, columns, rows_per_node))
 
     # Print all Nodes
     for obj in ListNodes_:
+        classify_df(obj, columns, rows)
         printNode(obj)
 
-
-    # DATA CONVERTING
-
-    # Convert the data to be able to classify
-    X = dfData.iloc[:, 0:columns-2]
-    y = dfData.iloc[:, columns-1]
-
-    # Extract the 2 most important columns
-    topD = SelectKBest(score_func=chi2, k=important_rows)
-    fit = topD.fit(X, y)
-    dfscores = pd.DataFrame(fit.scores_)
-    dfcolumns = pd.DataFrame(X.columns)
-    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
-    featureScores.columns = ['Dimensions', 'Score']  # naming the dataframe columns
-    x = featureScores.nlargest(important_rows, 'Score')  # print 10 best features
-    print x
-    m1 = x['Dimensions'].iloc[0]
-    max1 = m1[1:]
-    m2 = x['Dimensions'].iloc[1]
-    max2 = m2[1:]
-    print max1, max2
 
 
 if __name__ == "__main__":
